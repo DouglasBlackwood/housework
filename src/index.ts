1e4419150e6ea7a64fb2410ccdbd8c6acb397a42
Charly Laurent <charly.laurent@eureden.com>
2021-04-25 17:26:43 +0200

    create non-recurring task not done

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 class Task {
     title: string;
+    time_till_undone: number | undefined;
+    done_at: Date | undefined;
 
-    constructor(title: string) {
+    constructor(title: string, time_till_undone?: number, done_at?: Date) {
         this.title = title;
+        this.time_till_undone = time_till_undone;
+        this.done_at = done_at;
     }
+
+    
+    public get is_done() : boolean {
+        return this.done_at ? true : false;
+    }
+    
 }
 
 export { Task };
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,12 @@
 import { Task } from '../src';
 
-test('create task with title', () => {
+test('create non-recurring task not done', () => {
     let task = new Task('Chore');
 
     expect(task.title).toBe('Chore');
+    expect(task.time_till_undone).toBeUndefined();
+    expect(task.done_at).toBeUndefined();
+    expect(task.is_done).toBeFalsy();
 });
+
+test.todo('set task done');
