8a2e593e3ec0e3c4d7b21d14b7b2e2d9e9b7d99f
Charly Laurent <charly.laurent@eureden.com>
2021-04-25 17:45:15 +0200

    task constructor accept an object for optionnal parameters

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
 class Task {
     title: string;
-    time_till_undone: number | undefined;
-    done_at: Date | undefined;
+    time_till_undone?: number;
+    done_at?: Date;
 
-    constructor(title: string, time_till_undone?: number, done_at?: Date) {
+    constructor({title, time_till_undone, done_at}: TaskConstructorInterface) {
         this.title = title;
         this.time_till_undone = time_till_undone;
         this.done_at = done_at;
     }
 
-    
-    public get is_done() : boolean {
+    public get is_done(): boolean {
         return this.done_at ? true : false;
     }
-    
+
+}
+
+interface TaskConstructorInterface {
+    title: string;
+    time_till_undone?: number;
+    done_at?: Date;
 }
 
 export { Task };
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,7 @@
 import { Task } from '../src';
 
 test('create non-recurring task not done', () => {
-    let task = new Task('Chore');
+    let task = new Task({title: 'Chore'});
 
     expect(task.title).toBe('Chore');
     expect(task.time_till_undone).toBeUndefined();
@@ -9,4 +9,11 @@ test('create non-recurring task not done', () => {
     expect(task.is_done).toBeFalsy();
 });
 
-test.todo('set task done');
+test('create non-recurring task done', () => {
+    let task = new Task({title: 'Chore', done_at: new Date()});
+
+    expect(task.title).toBe('Chore');
+    expect(task.time_till_undone).toBeUndefined();
+    expect(task.done_at).toBeDefined();
+    expect(task.is_done).toBeTruthy();
+});
