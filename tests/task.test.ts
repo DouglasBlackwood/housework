2becc270326f9296df98e69f85ef345d4bbde71a
Charly Laurent <charly.laurent@eureden.com>
2021-05-01 09:04:07 +0200

    group task tests into a test suite

--- tests/task.test.ts
+++ tests/task.test.ts
@@ -1,97 +1,99 @@
 import { Task, TaskStatus } from '../src';
 
-test('create non-recurring task not done', () => {
-    let task = new Task({ title: 'Chore' });
+describe('task behaviour', () => {
+    test('create non-recurring task not done', () => {
+        let task = new Task({ title: 'Chore' });
 
-    expect(task.title).toBe('Chore');
-    expect(task.days_till_undone).toBeUndefined();
-    expect(task.done_at).toBeUndefined();
-    expect(task.is_done).toBeFalsy();
-    expect(task.status).toBe(TaskStatus.ToDo);
-});
+        expect(task.title).toBe('Chore');
+        expect(task.days_till_undone).toBeUndefined();
+        expect(task.done_at).toBeUndefined();
+        expect(task.is_done).toBeFalsy();
+        expect(task.status).toBe(TaskStatus.ToDo);
+    });
 
-test('create non-recurring task done', () => {
-    let done_at = new Date();
-    let task = new Task({ title: 'Chore', done_at: done_at });
+    test('create non-recurring task done', () => {
+        let done_at = new Date();
+        let task = new Task({ title: 'Chore', done_at: done_at });
 
-    expect(task.days_till_undone).toBeUndefined();
-    expect(task.done_at).toBe(done_at);
-    expect(task.is_done).toBeTruthy();
-    expect(task.status).toBe(TaskStatus.Done);
-});
+        expect(task.days_till_undone).toBeUndefined();
+        expect(task.done_at).toBe(done_at);
+        expect(task.is_done).toBeTruthy();
+        expect(task.status).toBe(TaskStatus.Done);
+    });
 
-test('create recurring task not done', () => {
-    let task = new Task({ title: 'Chore', days_till_undone: 1 });
+    test('create recurring task not done', () => {
+        let task = new Task({ title: 'Chore', days_till_undone: 1 });
 
-    expect(task.days_till_undone).toBe(1);
-    expect(task.done_at).toBeUndefined();
-    expect(task.is_done).toBeFalsy();
-    expect(task.status).toBe(TaskStatus.ToDo);
-});
+        expect(task.days_till_undone).toBe(1);
+        expect(task.done_at).toBeUndefined();
+        expect(task.is_done).toBeFalsy();
+        expect(task.status).toBe(TaskStatus.ToDo);
+    });
 
-test('create recurring task done', () => {
-    let done_at = new Date();
-    let task = new Task({ title: 'Chore', days_till_undone: 1, done_at: done_at });
+    test('create recurring task done', () => {
+        let done_at = new Date();
+        let task = new Task({ title: 'Chore', days_till_undone: 1, done_at: done_at });
 
-    expect(task.days_till_undone).toBe(1);
-    expect(task.done_at).toBe(done_at);
-    expect(task.is_done).toBeTruthy();
-    expect(task.status).toBe(TaskStatus.Done);
-});
+        expect(task.days_till_undone).toBe(1);
+        expect(task.done_at).toBe(done_at);
+        expect(task.is_done).toBeTruthy();
+        expect(task.status).toBe(TaskStatus.Done);
+    });
 
-test('create recurring task undone', () => {
-    let done_at = new Date();
-    done_at.setDate(done_at.getDate() - 1);
-    let task = new Task({ title: 'Chore', days_till_undone: 1, done_at: done_at });
+    test('create recurring task undone', () => {
+        let done_at = new Date();
+        done_at.setDate(done_at.getDate() - 1);
+        let task = new Task({ title: 'Chore', days_till_undone: 1, done_at: done_at });
 
-    expect(task.days_till_undone).toBe(1);
-    expect(task.done_at).toBe(done_at);
-    expect(task.is_done).toBeFalsy();
-    expect(task.status).toBe(TaskStatus.ToDo);
-});
+        expect(task.days_till_undone).toBe(1);
+        expect(task.done_at).toBe(done_at);
+        expect(task.is_done).toBeFalsy();
+        expect(task.status).toBe(TaskStatus.ToDo);
+    });
 
-test('undo task', () => {
-    let done_at = new Date();
-    let task = new Task({ title: 'Chore', done_at: done_at });
-    task.undo();
+    test('undo task', () => {
+        let done_at = new Date();
+        let task = new Task({ title: 'Chore', done_at: done_at });
+        task.undo();
 
-    expect(task.is_done).toBeFalsy();
-    expect(task.done_at).toBeUndefined();
-});
+        expect(task.is_done).toBeFalsy();
+        expect(task.done_at).toBeUndefined();
+    });
 
-test('set task done', () => {
-    let done_at = new Date();
-    let task = new Task({ title: 'Chore' });
-    task.setDoneAt(done_at);
+    test('set task done', () => {
+        let done_at = new Date();
+        let task = new Task({ title: 'Chore' });
+        task.setDoneAt(done_at);
 
-    expect(task.is_done).toBeTruthy();
-    expect(task.done_at).toStrictEqual(done_at);
-});
+        expect(task.is_done).toBeTruthy();
+        expect(task.done_at).toStrictEqual(done_at);
+    });
 
-test('create recurring task late', () => {
-    let done_at = new Date();
-    done_at.setDate(done_at.getDate() - 2);
-    let task = new Task({ title: 'Chore', days_till_undone: 1, done_at: done_at });
+    test('create recurring task late', () => {
+        let done_at = new Date();
+        done_at.setDate(done_at.getDate() - 2);
+        let task = new Task({ title: 'Chore', days_till_undone: 1, done_at: done_at });
 
-    expect(task.status).toBe(TaskStatus.Late);
-});
+        expect(task.status).toBe(TaskStatus.Late);
+    });
 
-test('task due every 1 day is late after 2 days', () => {
-    let done_at = new Date();
-    let task = new Task({ title: 'Chore', days_till_undone: 1, done_at: done_at });
+    test('task due every 1 day is late after 2 days', () => {
+        let done_at = new Date();
+        let task = new Task({ title: 'Chore', days_till_undone: 1, done_at: done_at });
 
-    let expected_late_at = new Date(done_at.getTime());
-    expected_late_at.setDate(done_at.getDate() + 2);
+        let expected_late_at = new Date(done_at.getTime());
+        expected_late_at.setDate(done_at.getDate() + 2);
 
-    expect(task.late_at).toStrictEqual(expected_late_at);
-})
+        expect(task.late_at).toStrictEqual(expected_late_at);
+    })
 
-test('task due every 2 day is late after 3 days', () => {
-    let done_at = new Date();
-    let task = new Task({ title: 'Chore', days_till_undone: 2, done_at: done_at });
+    test('task due every 2 day is late after 3 days', () => {
+        let done_at = new Date();
+        let task = new Task({ title: 'Chore', days_till_undone: 2, done_at: done_at });
 
-    let expected_late_at = new Date(done_at.getTime());
-    expected_late_at.setDate(done_at.getDate() + 3);
+        let expected_late_at = new Date(done_at.getTime());
+        expected_late_at.setDate(done_at.getDate() + 3);
 
-    expect(task.late_at).toStrictEqual(expected_late_at);
+        expect(task.late_at).toStrictEqual(expected_late_at);
+    })
 })
