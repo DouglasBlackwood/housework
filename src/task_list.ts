683087251702492fbce4959c50b9a9cafffa7d2e
Charly Laurent <charly.laurent@eureden.com>
2021-05-02 15:46:55 +0200

    refacto

--- src/task_list.ts
+++ src/task_list.ts
@@ -3,24 +3,32 @@ import { Iterator } from './iterator';
 
 class TaskList {
     private data: Task[] = [];
-
+    
     public get count(): number {
         return this.data.length;
     }
 
+    public get tasks() {
+        return this.data.slice();
+    }
+
     add(task: Task): void {
         this.data.push(task);
+        this.sort();
+    }
+    
+    sort(): void {
         this.data.sort((a, b) => {
             // Sort by status if status is different
             if (a.status !== b.status) {
                 return a.status - b.status;
             }
-
+    
             // Sort by undone_ratio if possible
             if (!a.is_done && !b.is_done && a.undone_ratio !== b.undone_ratio) {
                 return b.undone_ratio - a.undone_ratio;
             }
-
+    
             // Sort by title by default
             if (a.title < b.title) {
                 return -1;
@@ -35,10 +43,6 @@ class TaskList {
     remove(task: Task): void {
         const task_index = this.data.indexOf(task);
         this.data.splice(task_index, 1);
-    }
-
-    public get tasks() {
-        return this.data.slice();
     }
 
     getIterator(): TaskIterator {
